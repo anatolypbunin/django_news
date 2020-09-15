52c0f05d6338b7ba0d6be556f9c44a50f1e06862
anatolypbunin <70144161+anatolypbunin@users.noreply.github.com>
2020-09-15 23:12:51 +0300

    Update settings.py

--- news/news/settings.py
+++ news/news/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pe-$_$grw2n7@x*bsvia!pv6d%yq*q6uih$489g7n3soiw#d2*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
